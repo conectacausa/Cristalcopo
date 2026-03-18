d725d492e4453b634722b179e97da63f4e20c6ee
conectacausa <contato@conectacausa.com.br>
2026-03-18 17:46:47 -0300

    Create index.blade.php

---
+++ resources/views/gestao/cargos/cbo/index.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="content-header">
+    <div class="d-flex align-items-center">
+        <div class="me-auto">
+            <h4 class="page-title">CBO</h4>
+        </div>
+        <button onclick="abrirNovo()" class="btn bg-gradient-success w-200">Novo CBO</button>
+    </div>
+</div>
+
+<section class="content">
+
+<div class="box">
+    <div class="box-body">
+        <input type="text" id="filtro" class="form-control" placeholder="Descrição ou Código">
+    </div>
+</div>
+
+<div class="box">
+    <div class="box-body">
+        <div id="tabela"></div>
+    </div>
+</div>
+
+</section>
+
+@include('gestao.cargos.cbo.partials.modal')
+
+@endsection
+
+@section('scripts')
+
+<script>
+let filtroTimer;
+
+$('#filtro').on('keyup', function () {
+    clearTimeout(filtroTimer);
+    filtroTimer = setTimeout(() => {
+        carregarTabela();
+    }, 400);
+});
+
+function carregarTabela(page = 1) {
+    $.get('/cargos/cbo/list?page=' + page + '&filtro=' + $('#filtro').val(), function (data) {
+        $('#tabela').html(data);
+    });
+}
+
+function abrirNovo() {
+    $('#modalCbo').modal('show');
+    $('#formCbo')[0].reset();
+    $('#id').val('');
+}
+
+function editar(id, codigo, descricao) {
+    $('#modalCbo').modal('show');
+    $('#id').val(id);
+    $('#codigo_cbo').val(codigo);
+    $('#descricao_cbo').val(descricao);
+}
+
+function salvar() {
+    let id = $('#id').val();
+    let url = id ? '/cargos/cbo/update/' + id : '/cargos/cbo/store';
+
+    $.post(url, $('#formCbo').serialize(), function () {
+        $('#modalCbo').modal('hide');
+        toastr.success('Salvo com sucesso');
+        carregarTabela();
+    });
+}
+
+function excluir(id) {
+    swal({
+        title: "Confirmar exclusão?",
+        text: "Essa ação não poderá ser desfeita!",
+        type: "warning",
+        showCancelButton: true,
+    }, function () {
+        $.ajax({
+            url: '/cargos/cbo/delete/' + id,
+            type: 'DELETE',
+            data: {_token: '{{ csrf_token() }}'},
+            success: function () {
+                toastr.success('Excluído com sucesso');
+                carregarTabela();
+            }
+        });
+    });
+}
+
+$(document).on('click', '.pagination a', function(e){
+    e.preventDefault();
+    let page = $(this).attr('href').split('page=')[1];
+    carregarTabela(page);
+});
+
+$(document).ready(function () {
+    carregarTabela();
+});
+</script>
+
+@endsection
