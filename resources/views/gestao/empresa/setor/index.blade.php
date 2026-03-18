fe9c33c57917500a5e53a9bb3858e881d2414f9f
conectacausa <contato@conectacausa.com.br>
2026-03-18 18:37:55 -0300

    Update index.blade.php

--- resources/views/gestao/empresa/setor/index.blade.php
+++ resources/views/gestao/empresa/setor/index.blade.php
@@ -1,151 +1,257 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}">
 
-<title>Cristalcopo - Setor</title>
+    <title>Cristalcopo - Setor</title>
 
-<link rel="stylesheet" href="{{ asset('assets/css/vendors_css.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/skin_color.css') }}">
+    <!-- Vendors Style-->
+    <link rel="stylesheet" href="{{ asset('assets/css/vendors_css.css') }}">
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <!-- Style-->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/skin_color.css') }}">
+
+    <!-- Toastr CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 </head>
-
 <body class="hold-transition light-skin sidebar-mini theme-primary fixed">
 
 <div class="wrapper">
+    <div id="loader"></div>
 
-@include('layouts.includes.header')
-@include('layouts.includes.menu')
+    @include('layouts.includes.header')
 
-<div class="content-wrapper">
-<div class="container-full">
+    @include('layouts.includes.menu')
 
-<div class="content-header">
-<div class="d-flex align-items-center">
-<div class="me-auto">
-<h4 class="page-title">Setor</h4>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <div class="container-full">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="d-flex align-items-center">
+                    <div class="me-auto">
+                        <h4 class="page-title">Setor</h4>
+                        <div class="d-inline-block align-items-center">
+                            <nav>
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        <a href="#"><i class="mdi mdi-home-outline"></i></a>
+                                    </li>
+                                    <li class="breadcrumb-item">Empresa</li>
+                                    <li class="breadcrumb-item active">Setor</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                    <button type="button" class="waves-effect waves-light btn mb-5 bg-gradient-success w-200" onclick="abrirNovo()">
+                        Novo Setor
+                    </button>
+                </div>
+            </div>
+
+            <!-- Main content -->
+            <section class="content">
+                <!-- Filtros -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="box">
+                            <div class="box-header with-border">
+                                <h4 class="box-title">Filtros</h4>
+                            </div>
+                            <div class="box-body">
+                                <div class="row">
+                                    <div class="col-md-7">
+                                        <div class="form-group">
+                                            <label class="form-label">Nome</label>
+                                            <input type="text" id="filtro_nome" class="form-control" placeholder="Nome do setor">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-5">
+                                        <div class="form-group">
+                                            <label class="form-label">Filial</label>
+                                            <select id="filtro_filial" class="form-select">
+                                                <option value="">Todas as filiais</option>
+                                                @foreach($filiais as $filial)
+                                                    <option value="{{ $filial->id }}">{{ $filial->nome }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Tabela -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="box">
+                            <div class="box-header with-border">
+                                <h4 class="box-title">Setor</h4>
+                            </div>
+                            <div class="box-body">
+                                <div class="table-responsive" id="setores-table-container">
+                                    @include('gestao.empresa.setor.partials.table', ['dados' => collect()])
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+        </div>
+    </div>
+    <!-- /.content-wrapper -->
+
+    @include('layouts.includes.footer')
 </div>
-
-<button onclick="abrirNovo()" class="btn bg-gradient-success w-200">Novo Setor</button>
-</div>
-</div>
-
-<section class="content">
-
-<div class="box">
-<div class="box-body">
-<div class="row">
-
-<div class="col-md-7">
-<input type="text" id="nome" class="form-control" placeholder="Nome">
-</div>
-
-<div class="col-md-5">
-<select id="filial_id" class="form-control">
-<option value="">Todas Filiais</option>
-@foreach($filiais as $f)
-<option value="{{ $f->id }}">{{ $f->nome }}</option>
-@endforeach
-</select>
-</div>
-
-</div>
-</div>
-</div>
-
-<div class="box">
-<div class="box-body">
-<div id="tabela"></div>
-</div>
-</div>
-
-</section>
-
-</div>
-</div>
-
-@include('layouts.includes.footer')
-
-</div>
-
-<script src="{{ asset('assets/js/vendors.min.js') }}"></script>
-<script src="{{ asset('assets/vendor_components/sweetalert/sweetalert.min.js') }}"></script>
-<script src="{{ asset('assets/js/template.js') }}"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<!-- ./wrapper -->
 
 @include('gestao.empresa.setor.partials.modal')
 
+<!-- Vendor JS -->
+<script src="{{ asset('assets/js/vendors.min.js') }}"></script>
+<script src="{{ asset('assets/js/pages/chat-popup.js') }}"></script>
+<script src="{{ asset('assets/icons/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('assets/vendor_components/sweetalert/sweetalert.min.js') }}"></script>
+<script src="{{ asset('assets/vendor_components/sweetalert/jquery.sweet-alert.custom.js') }}"></script>
+<script src="{{ asset('assets/js/template.js') }}"></script>
+
+<!-- Toastr CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 <script>
-toastr.options = {closeButton:true,progressBar:true};
+    toastr.options = {
+        closeButton: true,
+        progressBar: true,
+        positionClass: "toast-top-right",
+        timeOut: "3000"
+    };
 
-let timer;
+    let filtroTimer = null;
 
-$('#nome, #filial_id').on('keyup change', function(){
-    clearTimeout(timer);
-    timer = setTimeout(carregarTabela, 300);
-});
+    function carregarTabela(page = 1) {
+        $('#setores-table-container').html('<div class="text-center p-20">Carregando...</div>');
 
-function carregarTabela(page=1){
-    $.get('/empresa/setor/list?page='+page+'&nome='+$('#nome').val()+'&filial_id='+$('#filial_id').val(), function(data){
-        $('#tabela').html(data);
-    });
-}
-
-function abrirNovo(){
-    $('#form')[0].reset();
-    $('#id').val('');
-    $('#modal').modal('show');
-}
-
-function editar(id,nome,filiais){
-    $('#modal').modal('show');
-    $('#id').val(id);
-    $('#nome_setor').val(nome);
-
-    $('input[name="filiais[]"]').prop('checked', false);
-    filiais.forEach(f => $('#filial_'+f).prop('checked', true));
-}
-
-function salvar(){
-    let id = $('#id').val();
-    let url = id ? '/empresa/setor/update/'+id : '/empresa/setor/store';
-
-    $.post(url,$('#form').serialize())
-    .done(()=>{
-        toastr.success('Salvo com sucesso');
-        $('#modal').modal('hide');
-        carregarTabela();
-    });
-}
-
-function excluir(id){
-    swal({
-        title:"Excluir?",
-        type:"warning",
-        showCancelButton:true
-    },function(){
         $.ajax({
-            url:'/empresa/setor/delete/'+id,
-            type:'DELETE',
-            data:{_token:'{{ csrf_token() }}'},
-            success:function(){
-                toastr.success('Excluído com sucesso');
-                carregarTabela();
+            url: "{{ url('/empresa/setor/list') }}",
+            type: "GET",
+            data: {
+                page: page,
+                nome: $('#filtro_nome').val(),
+                filial_id: $('#filtro_filial').val()
+            },
+            success: function(response) {
+                $('#setores-table-container').html(response);
+            },
+            error: function() {
+                toastr.error('Erro ao carregar setores.');
             }
         });
+    }
+
+    $('#filtro_nome').on('keyup', function () {
+        clearTimeout(filtroTimer);
+        filtroTimer = setTimeout(function () {
+            carregarTabela(1);
+        }, 300);
     });
-}
 
-$(document).on('click','.pagination a',function(e){
-    e.preventDefault();
-    let page=$(this).attr('href').split('page=')[1];
-    carregarTabela(page);
-});
+    $('#filtro_filial').on('change', function () {
+        carregarTabela(1);
+    });
 
-$(document).ready(carregarTabela);
+    function abrirNovo() {
+        $('#setor_id').val('');
+        $('#nome').val('');
+        $('input[name="filiais[]"]').prop('checked', false);
+        $('#modalSetorLabel').text('Novo Setor');
+        $('#modalSetor').modal('show');
+    }
+
+    function editar(id, nome, filiais) {
+        $('#setor_id').val(id);
+        $('#nome').val(nome);
+        $('input[name="filiais[]"]').prop('checked', false);
+
+        if (Array.isArray(filiais)) {
+            filiais.forEach(function(filialId) {
+                $('#filial_' + filialId).prop('checked', true);
+            });
+        }
+
+        $('#modalSetorLabel').text('Editar Setor');
+        $('#modalSetor').modal('show');
+    }
+
+    function salvar() {
+        let id = $('#setor_id').val();
+        let url = id
+            ? "{{ url('/empresa/setor/update') }}/" + id
+            : "{{ url('/empresa/setor/store') }}";
+
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: $('#formSetor').serialize(),
+            success: function() {
+                $('#modalSetor').modal('hide');
+                toastr.success('Setor salvo com sucesso.');
+                carregarTabela();
+            },
+            error: function() {
+                toastr.error('Erro ao salvar setor.');
+            }
+        });
+    }
+
+    function excluir(id) {
+        swal({
+            title: "Tem certeza?",
+            text: "Deseja realmente excluir este setor?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#2962FF",
+            confirmButtonText: "Sim, excluir",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: true
+        }, function(confirmado) {
+            if (confirmado) {
+                $.ajax({
+                    url: "{{ url('/empresa/setor/delete') }}/" + id,
+                    type: "DELETE",
+                    data: {
+                        _token: "{{ csrf_token() }}"
+                    },
+                    success: function() {
+                        toastr.success('Setor excluído com sucesso.');
+                        carregarTabela();
+                    },
+                    error: function() {
+                        toastr.error('Erro ao excluir setor.');
+                    }
+                });
+            }
+        });
+    }
+
+    $(document).on('click', '#setores-table-container .pagination a', function(e) {
+        e.preventDefault();
+        let href = $(this).attr('href');
+        let page = new URL(href).searchParams.get('page') || 1;
+        carregarTabela(page);
+    });
+
+    $(document).ready(function () {
+        carregarTabela();
+    });
 </script>
 
 </body>
