02372f50a97f52e5504add91d25bffc3936f6009
conectacausa <contato@conectacausa.com.br>
2026-03-17 23:23:12 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,11 @@ Route::prefix('auth')->group(function () {
         Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
         Route::post('/login', [LoginController::class, 'autenticar'])->name('auth.login.autenticar');
 
-        Route::view('/recuperar', 'auth.recuperar.index')->name('auth.recuperar');
-    });
-
-    Route::middleware(['auth', 'user.active', 'first.access.pending'])->group(function () {
         Route::get('/acesso', [AcessoController::class, 'index'])->name('auth.acesso');
         Route::post('/acesso/validar', [AcessoController::class, 'validarIdentidade'])->name('auth.acesso.validar');
         Route::post('/acesso', [AcessoController::class, 'registrar'])->name('auth.acesso.registrar');
+
+        Route::view('/recuperar', 'auth.recuperar.index')->name('auth.recuperar');
     });
 
     Route::middleware('auth')->group(function () {
@@ -29,8 +27,8 @@ Route::prefix('auth')->group(function () {
     });
 });
 
-Route::middleware(['auth', 'user.active', 'first.access.completed'])->group(function () {
+Route::middleware(['auth', 'user.active'])->group(function () {
     Route::get('/dashboard', function () {
         return 'Dashboard (logado)';
-    })->name('dashboard');
+    })->name('dashboard')->middleware('screen:dashboard');
 });
