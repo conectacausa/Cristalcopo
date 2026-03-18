8dbaa2793cf7f91bb79b3b763ea94786a55c114e
conectacausa <contato@conectacausa.com.br>
2026-03-17 23:27:41 -0300

    Update GestaoTela.php

--- app/Models/GestaoTela.php
+++ app/Models/GestaoTela.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class GestaoTela extends Model
 {
@@ -14,4 +15,9 @@ class GestaoTela extends Model
         'icone',
         'slug',
     ];
+
+    public function permissoesLogin(): HasMany
+    {
+        return $this->hasMany(GestaoPermissao::class, 'login_tela_id');
+    }
 }
