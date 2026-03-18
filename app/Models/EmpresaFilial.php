fe4810e855383f8553ebb5538c2a12abb6ad461a
conectacausa <contato@conectacausa.com.br>
2026-03-18 18:30:29 -0300

    Update EmpresaFilial.php

--- app/Models/EmpresaFilial.php
+++ app/Models/EmpresaFilial.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmpresaFilial extends Model
 {
@@ -15,61 +14,17 @@ class EmpresaFilial extends Model
     protected $fillable = [
         'razao_social',
         'cnpj',
-        'nome_fantasia',
-        'data_abertura',
-        'porte_id',
-        'natureza_juridica_id',
-        'tipo',
-        'situacao',
-        'telefone1',
-        'telefone2',
-        'email',
-        'logradouro',
-        'numero',
-        'bairro',
-        'cidade_id',
-        'estado_id',
-        'pais_id',
-        'complemento',
-        'cep',
+        'nome_fantasia'
     ];
 
-    protected function casts(): array
-    {
-        return [
-            'data_abertura' => 'date',
-            'porte_id' => 'integer',
-            'natureza_juridica_id' => 'integer',
-            'cidade_id' => 'integer',
-            'estado_id' => 'integer',
-            'pais_id' => 'integer',
-            'situacao' => 'boolean',
-            'deleted_at' => 'datetime',
-        ];
-    }
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESSOR PADRÃO
+    |--------------------------------------------------------------------------
+    */
 
-    public function porte(): BelongsTo
+    public function getNomeAttribute()
     {
-        return $this->belongsTo(EmpresaPorte::class, 'porte_id');
-    }
-
-    public function naturezaJuridica(): BelongsTo
-    {
-        return $this->belongsTo(EmpresaNatJuridica::class, 'natureza_juridica_id');
-    }
-
-    public function pais(): BelongsTo
-    {
-        return $this->belongsTo(GestaoPais::class, 'pais_id');
-    }
-
-    public function estado(): BelongsTo
-    {
-        return $this->belongsTo(GestaoEstado::class, 'estado_id');
-    }
-
-    public function cidade(): BelongsTo
-    {
-        return $this->belongsTo(GestaoCidade::class, 'cidade_id');
+        return $this->nome_fantasia;
     }
 }
