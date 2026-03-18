8473d891439c6162259d3c27fe9a54e2c2541d27
conectacausa <contato@conectacausa.com.br>
2026-03-17 22:04:34 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('auth')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+    Route::post('/login', [LoginController::class, 'autenticar'])->name('auth.login.autenticar');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+
+    Route::view('/recuperar', 'auth.recuperar.index')->name('auth.recuperar');
+    Route::view('/acesso', 'auth.acesso.index')->name('auth.acesso');
 });
