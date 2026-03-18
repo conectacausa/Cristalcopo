f6c2b38b6df98a402746cda70c0e22b09d202e33
conectacausa <contato@conectacausa.com.br>
2026-03-17 23:13:48 -0300

    Update EnsureFirstAccessCompleted.php

--- app/Http/Middleware/EnsureFirstAccessCompleted.php
+++ app/Http/Middleware/EnsureFirstAccessCompleted.php
@@ -20,11 +20,6 @@ class EnsureFirstAccessCompleted
         }
 
         if (empty($user->senha)) {
-            auth()->logout();
-
-            $request->session()->invalidate();
-            $request->session()->regenerateToken();
-
             return redirect()
                 ->route('auth.acesso')
                 ->with('warning', 'Você precisa concluir o primeiro acesso antes de entrar no sistema.');
