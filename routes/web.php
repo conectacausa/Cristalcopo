c5310506fc20369a89c3d87a94b3f794b22adb48
conectacausa <contato@conectacausa.com.br>
2026-03-17 23:06:01 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,24 @@ Route::get('/', function () {
 })->name('home');
 
 Route::prefix('auth')->group(function () {
-    Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
-    Route::post('/login', [LoginController::class, 'autenticar'])->name('auth.login.autenticar');
-    Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+    Route::middleware('guest')->group(function () {
+        Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+        Route::post('/login', [LoginController::class, 'autenticar'])->name('auth.login.autenticar');
 
-    Route::get('/acesso', [AcessoController::class, 'index'])->name('auth.acesso');
-    Route::post('/acesso/validar', [AcessoController::class, 'validarIdentidade'])->name('auth.acesso.validar');
-    Route::post('/acesso', [AcessoController::class, 'registrar'])->name('auth.acesso.registrar');
+        Route::get('/acesso', [AcessoController::class, 'index'])->name('auth.acesso');
+        Route::post('/acesso/validar', [AcessoController::class, 'validarIdentidade'])->name('auth.acesso.validar');
+        Route::post('/acesso', [AcessoController::class, 'registrar'])->name('auth.acesso.registrar');
 
-    Route::view('/recuperar', 'auth.recuperar.index')->name('auth.recuperar');
+        Route::view('/recuperar', 'auth.recuperar.index')->name('auth.recuperar');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+    });
 });
 
-Route::get('/dashboard', function () {
-    return 'Dashboard (logado)';
+Route::middleware(['auth', 'first.access.completed'])->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Dashboard (logado)';
+    })->name('dashboard');
 });
