a2cae030f1c86f2a46e4785405852675846e2ecf
conectacausa <contato@conectacausa.com.br>
2026-03-18 18:27:16 -0300

    Update modal.blade.php

--- resources/views/empresa/setor/partials/modal.blade.php
+++ resources/views/empresa/setor/partials/modal.blade.php
@@ -1,62 +1,43 @@
-<div class="modal fade" id="setorModal" tabindex="-1" aria-labelledby="setorModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
+<div class="modal fade" id="modal">
+<div class="modal-dialog">
+<div class="modal-content">
 
-            <form id="formSetor">
-                @csrf
+<div class="modal-header">
+<h4>Setor</h4>
+</div>
 
-                <input type="hidden" id="setor_id" name="setor_id">
+<div class="modal-body">
 
-                <div class="modal-header">
-                    <h5 class="modal-title" id="setorModalLabel">Novo Setor</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
-                </div>
+<form id="form">
+@csrf
+<input type="hidden" id="id" name="id">
 
-                <div class="modal-body">
-                    <div class="row">
+<div class="form-group">
+<label>Nome</label>
+<input type="text" name="nome" id="nome_setor" class="form-control">
+</div>
 
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label class="form-label">Descrição do Setor <span class="text-danger">*</span></label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    id="descricao"
-                                    name="descricao"
-                                    placeholder="Digite a descrição do setor"
-                                    maxlength="255"
-                                    required>
-                            </div>
-                        </div>
+<div class="form-group">
+<label>Filiais</label>
 
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label class="form-label">Filial <span class="text-danger">*</span></label>
-                                <select id="id_filial" name="id_filial" class="form-control" required>
-                                    <option value="">Selecione</option>
-                                    @foreach($filiais as $filial)
-                                        <option value="{{ $filial->id }}">
-                                            {{ $filial->nome }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
+@foreach(\App\Models\EmpresaFilial::all() as $f)
+<div>
+<input type="checkbox" id="filial_{{ $f->id }}" name="filiais[]" value="{{ $f->id }}">
+<label>{{ $f->nome }}</label>
+</div>
+@endforeach
 
-                    </div>
-                </div>
+</div>
 
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                        Cancelar
-                    </button>
+</form>
 
-                    <button type="submit" class="btn btn-success" id="btnSalvarSetor">
-                        Salvar
-                    </button>
-                </div>
-            </form>
+</div>
 
-        </div>
-    </div>
+<div class="modal-footer">
+<button onclick="salvar()" class="btn btn-success">Salvar</button>
+<button class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+</div>
+
+</div>
+</div>
 </div>
