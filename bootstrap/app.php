4100f9e14d0b5c4ca28ca785448034acc64e5279
conectacausa <contato@conectacausa.com.br>
2026-03-17 23:09:47 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\EnsureUserIsActive;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->redirectGuestsTo(function (Request $request) {
+            return route('auth.login');
+        });
+
         $middleware->alias([
             'first.access.completed' => EnsureFirstAccessCompleted::class,
             'user.active' => EnsureUserIsActive::class,
