f7aa006c6359a2d18216fcb658888d93162b1876
conectacausa <contato@conectacausa.com.br>
2026-03-18 18:44:32 -0300

    Update modal.blade.php

--- resources/views/gestao/empresa/setor/partials/modal.blade.php
+++ resources/views/gestao/empresa/setor/partials/modal.blade.php
@@ -1,11 +1,39 @@
-<div class="form-group">
-<label>Filiais</label>
+<div class="modal fade" id="modalSetor">
+    <div class="modal-dialog">
+        <div class="modal-content">
 
-@foreach(\App\Models\EmpresaFilial::orderBy('nome_fantasia')->get() as $f)
-<div>
-<input type="checkbox" id="filial_{{ $f->id }}" name="filiais[]" value="{{ $f->id }}">
-<label>{{ $f->nome }}</label>
-</div>
-@endforeach
+            <div class="modal-header">
+                <h4 class="modal-title">Setor</h4>
+            </div>
 
+            <div class="modal-body">
+                <form id="formSetor">
+                    @csrf
+                    <input type="hidden" id="id" name="id">
+
+                    <div class="form-group">
+                        <label>Nome</label>
+                        <input type="text" name="nome" id="nome_setor" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Filiais</label>
+
+                        @foreach(\App\Models\EmpresaFilial::orderBy('nome_fantasia')->get() as $filial)
+                            <div>
+                                <input type="checkbox" id="filial_{{ $filial->id }}" name="filiais[]" value="{{ $filial->id }}">
+                                <label for="filial_{{ $filial->id }}">{{ $filial->nome }}</label>
+                            </div>
+                        @endforeach
+                    </div>
+                </form>
+            </div>
+
+            <div class="modal-footer">
+                <button onclick="salvar()" class="btn btn-success">Salvar</button>
+                <button class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+            </div>
+
+        </div>
+    </div>
 </div>
