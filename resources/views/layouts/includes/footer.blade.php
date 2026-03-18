8b0c282df4fdcce0309ecf2ac23f39ee18da4464
conectacausa <contato@conectacausa.com.br>
2026-03-18 00:02:31 -0300

    Create footer.blade.php

---
+++ resources/views/layouts/includes/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Documentos</a>
+            </li>
+        </ul>
+    </div>
+
+    &copy; {{ date('Y') }}
+    <a href="https://www.conecttacausa.com.br" target="_blank" rel="noopener noreferrer">
+        Conectta Causa
+    </a>.
+    Todos os Direitos Reservados!
+</footer>
