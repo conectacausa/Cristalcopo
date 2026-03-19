a446b94ccd5cdf842df2f4a9a82bee73036f4c7e
conectacausa <contato@conectacausa.com.br>
2026-03-19 16:16:41 -0300

    Update modal.blade.php

--- resources/views/gestao/empresa/setor/partials/modal.blade.php
+++ resources/views/gestao/empresa/setor/partials/modal.blade.php
@@ -1,7 +1,6 @@
-<div class="modal fade" id="modalSetor">
+<div class="modal fade" id="modalSetor" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-
             <div class="modal-header">
                 <h4 class="modal-title">Setor</h4>
             </div>
@@ -9,20 +8,28 @@
             <div class="modal-body">
                 <form id="formSetor">
                     @csrf
-                    <input type="hidden" id="id" name="id">
+                    <input type="hidden" id="setor_id" name="id">
 
                     <div class="form-group">
-                        <label>Setor</label>
-                        <input type="text" name="descricao" id="descricao_setor" class="form-control">
+                        <label class="form-label">Setor</label>
+                        <input type="text" class="form-control" id="descricao" name="descricao">
                     </div>
 
                     <div class="form-group">
-                        <label>Filiais</label>
+                        <label class="form-label">Filiais</label>
 
                         @foreach(\App\Models\EmpresaFilial::orderBy('nome_fantasia')->get() as $filial)
-                            <div>
-                                <input type="checkbox" id="filial_{{ $filial->id }}" name="filiais[]" value="{{ $filial->id }}">
-                                <label for="filial_{{ $filial->id }}">{{ $filial->nome }}</label>
+                            <div class="form-check mb-5">
+                                <input
+                                    class="form-check-input"
+                                    type="checkbox"
+                                    id="filial_{{ $filial->id }}"
+                                    name="filiais[]"
+                                    value="{{ $filial->id }}"
+                                >
+                                <label class="form-check-label" for="filial_{{ $filial->id }}">
+                                    {{ $filial->nome }}
+                                </label>
                             </div>
                         @endforeach
                     </div>
@@ -33,7 +40,6 @@
                 <button type="button" onclick="salvar()" class="btn btn-success">Salvar</button>
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
             </div>
-
         </div>
     </div>
 </div>
