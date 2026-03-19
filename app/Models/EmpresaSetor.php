0934b49a191232f578b9ac4e4a402d70abef0ac0
conectacausa <contato@conectacausa.com.br>
2026-03-19 15:36:14 -0300

    Update EmpresaSetor.php

--- app/Models/EmpresaSetor.php
+++ app/Models/EmpresaSetor.php
@@ -5,13 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class EmpresaSetor extends Model
+class Setor extends Model
 {
     use SoftDeletes;
 
     protected $table = 'empresa_setores';
 
     protected $fillable = [
-        'descricao'
+        'descricao',
     ];
+
+    protected function casts(): array
+    {
+        return [
+            'deleted_at' => 'datetime',
+        ];
+    }
+
+    public function getNomeAttribute()
+    {
+        return $this->descricao;
+    }
+
+    public function filiais()
+    {
+        return $this->belongsToMany(
+            EmpresaFilial::class,
+            'vinculo_filial_x_setor',
+            'setor_id',
+            'filial_id'
+        );
+    }
 }
