f9528a70cd42a2cfd47cf54fb4c2f8467ae6ea6c
conectacausa <contato@conectacausa.com.br>
2026-03-19 16:15:44 -0300

    Update EmpresaFilial.php

--- app/Models/EmpresaFilial.php
+++ app/Models/EmpresaFilial.php
@@ -5,79 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class EmpresaFilial extends Model
+class EmpresaSetor extends Model
 {
     use SoftDeletes;
 
-    protected $table = 'empresa_filial';
+    protected $table = 'empresa_setores';
 
     protected $fillable = [
-        'razao_social',
-        'nome_fantasia',
-        'cnpj',
-        'data_abertura',
-        'porte_id',
-        'nat_juridica_id',
-        'tipo',
-        'situacao',
-        'telefone1',
-        'telefone2',
-        'email',
-        'logradouro',
-        'numero',
-        'bairro',
-        'cidade_id',
-        'estado_id',
-        'pais_id',
-        'complemento',
+        'descricao',
     ];
 
     protected function casts(): array
     {
         return [
-            'data_abertura' => 'date',
-            'situacao' => 'boolean',
             'deleted_at' => 'datetime',
         ];
     }
 
     public function getNomeAttribute()
     {
-        return $this->nome_fantasia;
+        return $this->descricao;
     }
 
-    public function porte()
-    {
-        return $this->belongsTo(EmpresaPorte::class, 'porte_id');
-    }
-
-    public function naturezaJuridica()
-    {
-        return $this->belongsTo(EmpresaNaturezaJuridica::class, 'nat_juridica_id');
-    }
-
-    public function pais()
-    {
-        return $this->belongsTo(GestaoPais::class, 'pais_id');
-    }
-
-    public function estado()
-    {
-        return $this->belongsTo(GestaoEstado::class, 'estado_id');
-    }
-
-    public function cidade()
-    {
-        return $this->belongsTo(GestaoCidade::class, 'cidade_id');
-    }
-
-    public function setores()
+    public function filiais()
     {
         return $this->belongsToMany(
-            EmpresaSetor::class,
+            EmpresaFilial::class,
             'vinculo_filial_x_setor',
-            'filial_id',
-            'setor_id'
+            'setor_id',
+            'filial_id'
         );
     }
 }
