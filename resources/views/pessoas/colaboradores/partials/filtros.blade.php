622c15e41be51a30bb6393af8dee80c2eb19851a
conectacausa <contato@conectacausa.com.br>
2026-03-21 12:17:02 -0300

    Update filtros.blade.php

--- resources/views/pessoas/colaboradores/partials/filtros.blade.php
+++ resources/views/pessoas/colaboradores/partials/filtros.blade.php
@@ -7,55 +7,16 @@
             <div class="box-body">
                 <form id="form-filtros" action="{{ route('pessoas.colaboradores.index') }}" method="GET">
                     <div class="row">
-                        <div class="col-md-3">
+                        <div class="col-md-9">
                             <div class="form-group">
                                 <label class="form-label">Nome, Matrícula ou CPF</label>
                                 <input
                                     type="text"
                                     name="texto"
                                     class="form-control"
-                                    placeholder="Digite para buscar"
+                                    placeholder="Digite nome, matrícula ou CPF"
                                     value="{{ $filtros['texto'] ?? '' }}"
                                 >
-                            </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="form-group">
-                                <label class="form-label">Filiais</label>
-                                <select name="filiais[]" class="form-control select2" multiple>
-                                    @foreach($filiaisLista as $filial)
-                                        <option value="{{ $filial->id }}" @selected(in_array($filial->id, $filtros['filiais'] ?? []))>
-                                            {{ $filial->nome_fantasia }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="form-group">
-                                <label class="form-label">Setores</label>
-                                <select name="setores[]" class="form-control select2" multiple>
-                                    @foreach($setoresLista as $setor)
-                                        <option value="{{ $setor->id }}" @selected(in_array($setor->id, $filtros['setores'] ?? []))>
-                                            {{ $setor->descricao }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="form-group">
-                                <label class="form-label">Cargos</label>
-                                <select name="cargos[]" class="form-control select2" multiple>
-                                    @foreach($cargosLista as $cargo)
-                                        <option value="{{ $cargo->id }}" @selected(in_array($cargo->id, $filtros['cargos'] ?? []))>
-                                            {{ $cargo->titulo_cargo }}
-                                        </option>
-                                    @endforeach
-                                </select>
                             </div>
                         </div>
 
@@ -66,6 +27,47 @@
                                     <option value="ativo" @selected(($filtros['situacao'] ?? 'ativo') === 'ativo')>Ativo</option>
                                     <option value="inativo" @selected(($filtros['situacao'] ?? '') === 'inativo')>Inativo</option>
                                     <option value="todos" @selected(($filtros['situacao'] ?? '') === 'todos')>Todos</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label class="form-label">Filiais</label>
+                                <select name="filiais[]" class="form-control select2" multiple data-placeholder="Selecione as filiais">
+                                    @foreach($filiaisLista as $filial)
+                                        <option value="{{ $filial->id }}" @selected(in_array($filial->id, $filtros['filiais'] ?? []))>
+                                            {{ $filial->nome_fantasia }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label class="form-label">Setores</label>
+                                <select name="setores[]" class="form-control select2" multiple data-placeholder="Selecione os setores">
+                                    @foreach($setoresLista as $setor)
+                                        <option value="{{ $setor->id }}" @selected(in_array($setor->id, $filtros['setores'] ?? []))>
+                                            {{ $setor->descricao }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label class="form-label">Cargos</label>
+                                <select name="cargos[]" class="form-control select2" multiple data-placeholder="Selecione os cargos">
+                                    @foreach($cargosLista as $cargo)
+                                        <option value="{{ $cargo->id }}" @selected(in_array($cargo->id, $filtros['cargos'] ?? []))>
+                                            {{ $cargo->titulo_cargo }}
+                                        </option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
