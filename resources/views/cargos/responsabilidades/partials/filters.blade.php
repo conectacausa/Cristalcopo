2287ada3e53c56758392035d3da0f6f3e6910c08
conectacausa <contato@conectacausa.com.br>
2026-03-21 10:36:41 -0300

    Create filters.blade.php

---
+++ resources/views/cargos/responsabilidades/partials/filters.blade.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="col-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <h4 class="box-title">Filtros</h4>
+            </div>
+            <div class="box-body">
+                <form id="form-filtros">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label class="form-label">Descrição</label>
+                                <input type="text" class="form-control" id="busca" name="busca" placeholder="Digite a descrição">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
