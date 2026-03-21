6419c536dc37f3df32b2ad9d0bd65e0002d053e5
conectacausa <contato@conectacausa.com.br>
2026-03-20 22:46:33 -0300

    Create modal-show.blade.php

---
+++ resources/views/cargos/cargos/partials/modal-show.blade.php
@@ -0,0 +1,58 @@
+<div class="modal fade" id="modal-show-cargo" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Detalhes do Cargo</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+            </div>
+
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-6 mb-10">
+                        <strong>Título do Cargo:</strong>
+                        <div id="show_titulo_cargo">-</div>
+                    </div>
+
+                    <div class="col-md-6 mb-10">
+                        <strong>Código Importação:</strong>
+                        <div id="show_codigo_importacao">-</div>
+                    </div>
+
+                    <div class="col-md-12 mb-10">
+                        <strong>CBO:</strong>
+                        <div id="show_cbo">-</div>
+                    </div>
+
+                    <div class="col-md-4 mb-10">
+                        <strong>Status Aprovação:</strong>
+                        <div id="show_status">-</div>
+                    </div>
+
+                    <div class="col-md-4 mb-10">
+                        <strong>Jovem Aprendiz:</strong>
+                        <div id="show_jovem_aprendiz">-</div>
+                    </div>
+
+                    <div class="col-md-4 mb-10">
+                        <strong>Solicitação Aprovação:</strong>
+                        <div id="show_aprovacao_solicitacao_id">-</div>
+                    </div>
+
+                    <div class="col-md-12 mb-10">
+                        <strong>Filiais:</strong>
+                        <div id="show_filiais">-</div>
+                    </div>
+
+                    <div class="col-md-12 mb-10">
+                        <strong>Setores:</strong>
+                        <div id="show_setores">-</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary-light" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
