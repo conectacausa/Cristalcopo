ba04f606bef3e7d212be145da0fa9ae708e9013f
conectacausa <contato@conectacausa.com.br>
2026-03-21 12:22:33 -0300

    Update index.blade.php

--- resources/views/pessoas/colaboradores/index.blade.php
+++ resources/views/pessoas/colaboradores/index.blade.php
@@ -5,37 +5,28 @@
 @section('content')
 <div class="content-wrapper">
     <div class="container-full">
+
         <div class="content-header">
             <div class="d-flex align-items-center">
                 <div class="me-auto">
                     <h4 class="page-title">Colaboradores</h4>
-                    <div class="d-inline-block align-items-center">
-                        <nav>
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#"><i class="mdi mdi-home-outline"></i></a></li>
-                                <li class="breadcrumb-item">Pessoas</li>
-                                <li class="breadcrumb-item active" aria-current="page">Colaboradores</li>
-                            </ol>
-                        </nav>
-                    </div>
                 </div>
-                @if($permissao->pode_gravar)
-                    <a href="#" class="waves-effect waves-light btn mb-5 bg-gradient-success w-200">
-                        Novo Colaborador
-                    </a>
-                @endif
             </div>
         </div>
 
         <section class="content">
+
+            {{-- FILTROS --}}
             @include('pessoas.colaboradores.partials.filtros')
 
+            {{-- TABELA --}}
             <div class="row">
                 <div class="col-12">
                     <div class="box">
                         <div class="box-header with-border">
-                            <h4 class="box-title">Listagem de Colaboradores</h4>
+                            <h4 class="box-title">Listagem</h4>
                         </div>
+
                         <div class="box-body">
                             <div id="resultado-tabela">
                                 @include('pessoas.colaboradores.partials.tabela', [
@@ -47,69 +38,83 @@
                     </div>
                 </div>
             </div>
+
         </section>
     </div>
 </div>
 @endsection
 
 @push('scripts')
-<script>
-$(document).ready(function () {
-    $('.select2').select2({
-        width: '100%'
-    });
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-    let filtroTimeout = null;
+<script>
+$(function () {
+
+    $('.select2').select2({ width: '100%' });
 
     function atualizarTabela(url = null) {
         const form = $('#form-filtros');
-        const targetUrl = url || form.attr('action');
 
-        $.ajax({
-            url: targetUrl,
-            method: 'GET',
-            data: form.serialize(),
-            success: function (html) {
-                $('#resultado-tabela').html(html);
-            },
-            error: function () {
-                toastr.error('Erro ao atualizar a tabela.');
-            }
+        $.get(url || form.attr('action'), form.serialize(), function (html) {
+            $('#resultado-tabela').html(html);
+        }).fail(function () {
+            toastr.error('Erro ao atualizar tabela');
         });
     }
 
-    $('#form-filtros').on('input change', 'input, select', function () {
-        clearTimeout(filtroTimeout);
-        filtroTimeout = setTimeout(function () {
-            atualizarTabela();
-        }, 300);
+    function carregarSetores() {
+        const filiais = $('select[name="filiais[]"]').val();
+
+        $.get("{{ route('pessoas.colaboradores.setores') }}", { filiais }, function (data) {
+
+            let select = $('select[name="setores[]"]');
+            select.empty();
+
+            data.forEach(item => {
+                select.append(new Option(item.descricao, item.id));
+            });
+
+            select.trigger('change.select2');
+        });
+    }
+
+    function carregarCargos() {
+        const setores = $('select[name="setores[]"]').val();
+
+        $.get("{{ route('pessoas.colaboradores.cargos') }}", { setores }, function (data) {
+
+            let select = $('select[name="cargos[]"]');
+            select.empty();
+
+            data.forEach(item => {
+                select.append(new Option(item.titulo_cargo, item.id));
+            });
+
+            select.trigger('change.select2');
+        });
+    }
+
+    $('#form-filtros').on('change', 'select[name="filiais[]"]', function () {
+        carregarSetores();
+        $('select[name="cargos[]"]').empty();
+        atualizarTabela();
     });
 
-    $(document).on('click', '#resultado-tabela .pagination a', function (e) {
+    $('#form-filtros').on('change', 'select[name="setores[]"]', function () {
+        carregarCargos();
+        atualizarTabela();
+    });
+
+    $('#form-filtros').on('change input', 'input, select[name="cargos[]"], select[name="situacao"]', function () {
+        atualizarTabela();
+    });
+
+    $(document).on('click', '.pagination a', function (e) {
         e.preventDefault();
         atualizarTabela($(this).attr('href'));
     });
 
-    $(document).on('click', '.btn-excluir-colaborador', function (e) {
-        e.preventDefault();
-
-        const form = $(this).closest('form');
-
-        swal({
-            title: 'Excluir colaborador?',
-            text: 'Esta ação fará exclusão lógica do registro.',
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sim, excluir',
-            cancelButtonText: 'Cancelar'
-        }, function (isConfirm) {
-            if (isConfirm) {
-                form.submit();
-            }
-        });
-    });
 });
 </script>
 @endpush
