41761147f978b10f47ab6099e45042d54552e41f
conectacausa <contato@conectacausa.com.br>
2026-03-21 09:18:20 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,41 @@
 <?php
 
-use App\Http\Controllers\Auth\AcessoController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Gestao\Empresa\EmpresaFilialController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CargoCboController;
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\AcessoController;
+
+use App\Http\Controllers\Gestao\Empresa\EmpresaFilialController;
 use App\Http\Controllers\Empresa\SetorController;
-use App\Http\Controllers\TesteAprovacaoController;
+use App\Http\Controllers\CargoCboController;
+
 use App\Http\Controllers\Aprovacao\FluxoAprovacaoController;
 use App\Http\Controllers\Aprovacao\ConfiguracaoFluxoController;
+
 use App\Http\Controllers\Cargos\CargosController;
 use App\Http\Controllers\Sst\RiscosController;
 
+/*
+|--------------------------------------------------------------------------
+| HOME
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
     return auth()->check()
         ? redirect()->route('auth.redirect')
         : redirect()->route('auth.login');
 })->name('home');
 
+
+/*
+|--------------------------------------------------------------------------
+| AUTH
+|--------------------------------------------------------------------------
+*/
 Route::prefix('auth')->group(function () {
+
     Route::middleware('guest')->group(function () {
+
         Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
         Route::post('/login', [LoginController::class, 'autenticar'])->name('auth.login.autenticar');
 
@@ -36,15 +52,34 @@ Route::prefix('auth')->group(function () {
     });
 });
 
+
+/*
+|--------------------------------------------------------------------------
+| SISTEMA (LOGADO)
+|--------------------------------------------------------------------------
+*/
 Route::middleware(['auth', 'user.active'])->group(function () {
+
+    /*
+    |--------------------------------------------------------------------------
+    | DASHBOARD
+    |--------------------------------------------------------------------------
+    */
     Route::view('/dashboard', 'dashboard.index')
         ->name('dashboard')
         ->middleware('screen:dashboard');
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | FILIAIS
+    |--------------------------------------------------------------------------
+    */
     Route::prefix('empresa/filiais')
         ->name('empresa.filiais.')
         ->middleware('screen:empresa/filiais')
         ->group(function () {
+
             Route::get('/', [EmpresaFilialController::class, 'index'])->name('index');
             Route::get('/create', [EmpresaFilialController::class, 'create'])->name('create');
             Route::post('/', [EmpresaFilialController::class, 'store'])->name('store');
@@ -64,63 +99,115 @@ Route::middleware(['auth', 'user.active'])->group(function () {
             Route::patch('/cnaes/{vinculoId}/principal', [EmpresaFilialController::class, 'atualizarPrincipalCnae'])->name('cnae.principal');
             Route::delete('/cnaes/{vinculoId}', [EmpresaFilialController::class, 'removerCnae'])->name('cnae.remover');
         });
-    Route::middleware(['auth', 'screen:cargos/cbo'])->group(function () {
-        Route::get('/cargos/cbo', [CargoCboController::class, 'index']);
-        Route::get('/cargos/cbo/list', [CargoCboController::class, 'list']);
-        Route::post('/cargos/cbo/store', [CargoCboController::class, 'store']);
-        Route::post('/cargos/cbo/update/{id}', [CargoCboController::class, 'update']);
-        Route::delete('/cargos/cbo/delete/{id}', [CargoCboController::class, 'delete']);
-    });
-   Route::middleware(['auth', 'user.active', 'screen:empresa/setor'])->group(function () {
-        Route::get('/empresa/setor', [SetorController::class, 'index'])->name('empresa.setor.index');
-        Route::get('/empresa/setor/list', [SetorController::class, 'list'])->name('empresa.setor.list');
-        Route::post('/empresa/setor/store', [SetorController::class, 'store'])->name('empresa.setor.store');
-        Route::post('/empresa/setor/update/{id}', [SetorController::class, 'update'])->name('empresa.setor.update');
-        Route::delete('/empresa/setor/delete/{id}', [SetorController::class, 'delete'])->name('empresa.setor.delete');
-    });
-   Route::prefix('configuracao/aprovacao')
-    ->name('aprovacao.fluxo.')
-    ->middleware('screen:configuracao/aprovacao')
-    ->group(function () {
-        Route::get('/', [FluxoAprovacaoController::class, 'index'])->name('index');
-        Route::get('/create', [FluxoAprovacaoController::class, 'create'])->name('create');
-        Route::post('/store', [FluxoAprovacaoController::class, 'store'])->name('store');
-        Route::get('/edit/{id}', [FluxoAprovacaoController::class, 'edit'])->name('edit');
-        Route::post('/update/{id}', [FluxoAprovacaoController::class, 'update'])->name('update');
-        Route::post('/delete/{id}', [FluxoAprovacaoController::class, 'destroy'])->name('delete');
-    });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | CBO
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('cargos/cbo')
+        ->middleware('screen:cargos/cbo')
+        ->group(function () {
+
+            Route::get('/', [CargoCboController::class, 'index']);
+            Route::get('/list', [CargoCboController::class, 'list']);
+            Route::post('/store', [CargoCboController::class, 'store']);
+            Route::post('/update/{id}', [CargoCboController::class, 'update']);
+            Route::delete('/delete/{id}', [CargoCboController::class, 'delete']);
+        });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | SETORES
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('empresa/setor')
+        ->name('empresa.setor.')
+        ->middleware('screen:empresa/setor')
+        ->group(function () {
+
+            Route::get('/', [SetorController::class, 'index'])->name('index');
+            Route::get('/list', [SetorController::class, 'list'])->name('list');
+            Route::post('/store', [SetorController::class, 'store'])->name('store');
+            Route::post('/update/{id}', [SetorController::class, 'update'])->name('update');
+            Route::delete('/delete/{id}', [SetorController::class, 'delete'])->name('delete');
+        });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | APROVAÇÃO - FLUXOS
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('configuracao/aprovacao')
+        ->name('aprovacao.fluxo.')
+        ->middleware('screen:configuracao/aprovacao')
+        ->group(function () {
+
+            Route::get('/', [FluxoAprovacaoController::class, 'index'])->name('index');
+            Route::get('/create', [FluxoAprovacaoController::class, 'create'])->name('create');
+            Route::post('/store', [FluxoAprovacaoController::class, 'store'])->name('store');
+            Route::get('/edit/{id}', [FluxoAprovacaoController::class, 'edit'])->name('edit');
+            Route::post('/update/{id}', [FluxoAprovacaoController::class, 'update'])->name('update');
+            Route::post('/delete/{id}', [FluxoAprovacaoController::class, 'destroy'])->name('delete');
+        });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | APROVAÇÃO - CONFIGURAÇÃO
+    |--------------------------------------------------------------------------
+    */
     Route::prefix('configuracao/aprovacao-config')
-    ->name('aprovacao.configuracao.')
-    ->middleware('screen:configuracao/aprovacao')
-    ->group(function () {
-        Route::get('/', [ConfiguracaoFluxoController::class, 'index'])->name('index');
-        Route::post('/store', [ConfiguracaoFluxoController::class, 'store'])->name('store');
-    });
-    Route::middleware(['auth', 'user.active'])->group(function () {
-        Route::prefix('sst/riscos')
-            ->name('sst.riscos.')
-            ->middleware('screen:sst/riscos')
-            ->group(function () {
-                Route::get('/', [RiscosController::class, 'index'])->name('index');
-                Route::get('/list', [RiscosController::class, 'list'])->name('list');
-                Route::post('/store', [RiscosController::class, 'store'])->name('store');
-                Route::get('/edit/{id}', [RiscosController::class, 'edit'])->name('edit');
-                Route::post('/update/{id}', [RiscosController::class, 'update'])->name('update');
-                Route::delete('/delete/{id}', [RiscosController::class, 'delete'])->name('delete');
-            });
-    
-        Route::prefix('cargos')
-            ->name('cargos.cargos.')
-            ->middleware('screen:cargos')
-            ->group(function () {
-                Route::get('/', [CargosController::class, 'index'])->name('index');
-                Route::get('/list', [CargosController::class, 'list'])->name('list');
-                Route::get('/create', [CargosController::class, 'create'])->name('create');
-                Route::post('/store', [CargosController::class, 'store'])->name('store');
-                Route::get('/edit/{id}', [CargosController::class, 'editPage'])->name('edit');
-                Route::post('/update/{id}', [CargosController::class, 'update'])->name('update');
-                Route::get('/show/{id}', [CargosController::class, 'show'])->name('show');
-                Route::get('/setores-por-filiais', [CargosController::class, 'setoresPorFiliais'])->name('setores_por_filiais');
-                Route::delete('/delete/{id}', [CargosController::class, 'delete'])->name('delete');
-            });
+        ->name('aprovacao.configuracao.')
+        ->middleware('screen:configuracao/aprovacao')
+        ->group(function () {
+
+            Route::get('/', [ConfiguracaoFluxoController::class, 'index'])->name('index');
+            Route::post('/store', [ConfiguracaoFluxoController::class, 'store'])->name('store');
+        });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | SST - RISCOS
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('sst/riscos')
+        ->name('sst.riscos.')
+        ->middleware('screen:sst/riscos')
+        ->group(function () {
+
+            Route::get('/', [RiscosController::class, 'index'])->name('index');
+            Route::get('/list', [RiscosController::class, 'list'])->name('list');
+            Route::post('/store', [RiscosController::class, 'store'])->name('store');
+            Route::get('/edit/{id}', [RiscosController::class, 'edit'])->name('edit');
+            Route::post('/update/{id}', [RiscosController::class, 'update'])->name('update');
+            Route::delete('/delete/{id}', [RiscosController::class, 'delete'])->name('delete');
+        });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | CARGOS
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('cargos')
+        ->name('cargos.cargos.')
+        ->middleware('screen:cargos')
+        ->group(function () {
+
+            Route::get('/', [CargosController::class, 'index'])->name('index');
+            Route::get('/list', [CargosController::class, 'list'])->name('list');
+            Route::get('/create', [CargosController::class, 'create'])->name('create');
+            Route::post('/store', [CargosController::class, 'store'])->name('store');
+            Route::get('/edit/{id}', [CargosController::class, 'editPage'])->name('edit');
+            Route::post('/update/{id}', [CargosController::class, 'update'])->name('update');
+            Route::get('/show/{id}', [CargosController::class, 'show'])->name('show');
+            Route::get('/setores-por-filiais', [CargosController::class, 'setoresPorFiliais'])->name('setores_por_filiais');
+            Route::delete('/delete/{id}', [CargosController::class, 'delete'])->name('delete');
+        });
+
 });
